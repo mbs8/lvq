3bcb983e7c643d136c85ab85534fdea0e4a1bcef
Branco <mbs8@cin.ufpe.br>
2019-04-22 00:55:13 -0300

    implementa o esqueleto de lvq1

--- lvq1.py
+++ lvq1.py
@@ -1,20 +1,43 @@
 from selection import selection
+from knn import knn
+import time
 
-def lvq1(dataset):
-    classes = []
-    prototypes = []
-    minArg = []
-    maxArg = []
-    prototypesPerClass = 10
+def adjustParam(startTime, testInstance, trainSetInstance):
+	currentTime = time.time() - startTime
+	func = -1 * (currentTime ** 2)
 
-    classes, prototypes, minArg, maxArg = selection(dataset, prototypesPerClass)
-    
+def lvq1(dataset, prototypesPerClass, k):
+	classes = []
+	prototypes = []
+	minArg = []
+	maxArg = []
+	tests = []
 
+	classes, prototypes, minArg, maxArg = selection(dataset, prototypesPerClass)
+
+	for protClas in prototypes:
+		for prototype in protClas:
+			tests.append(prototype)
+	
+	for test in tests:
+		knnClassification = knn(classes, tests, minArg, maxArg, test, k)
+		if knnClassification == test.classification:
+			# Arproxima o prototipo da instancia de treino
+			pass
+		else: 
+			# Afasta o prototipo da instancia de treino
+			pass
+
+	return tests
+
+			
 
 def main():
 	datasets = ["Datasets/CM1_software_defect_prediction.csv", "Datasets/KC2_software_defect_prediction.csv"]
+	k = 1
+	prototypesPerClass = 10
 
 	for dataset in datasets:
-		lvq1(dataset)
+		lvq1(dataset, prototypesPerClass, k)
 
 main()
